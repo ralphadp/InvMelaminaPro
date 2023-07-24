b09cac8dc6b251919f2cbd72ea9af70d30c9d655
ralph <ralphadp@gmail.com>
2023-07-24 14:05:34 -0400

    chart fix version

--- public/javascripts/red.js
+++ public/javascripts/red.js
@@ -107,7 +107,7 @@ function getProductoXDia(dia) {
         if (response.ok) {
             console.log(response.message);
             clearSVG()
-            BarsData(response.chartData, "venta_dia_bars");
+            BarsHorizontal(response.chartData, "venta_dia_bars");
             PieLegend(response.chartData, "venta_dia_pie");
         } else {
             console.log(response.status, response.statusText);
@@ -283,8 +283,12 @@ function BarsData(chartData, canvas_id) {
     svg.call(tip);
 
     // The following code was contained in the callback function.
-    x.domain(chartData.map(function(d) { return d.cliente; }));
-    y.domain([0, d3.max(chartData, function(d) { return d.Volume; })]);
+    x.domain(chartData.map(function(d) { 
+        return d.cliente; 
+    }));
+    y.domain([0, d3.max(chartData, function(d) { 
+        return d.Volume; 
+    })]);
 
     svg.append("g")
         .attr("class", "x axis")
@@ -520,6 +524,133 @@ function PieLegend(chartData, canvas_id) {
             .attr("text-anchor", "middle")
             .text(function (d, i) { return ((d.Volume / sum) * 100).toFixed(0) + "%"; });
     });
+}
+
+function BarsHorizontal(chartData, canvas_id) {
+        var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+            return "<strong style='color:red'>Volumen:</strong> <span style='color:red'>" + d.Volume + "</span>";
+        })
+
+        var outerWidth = 575;
+        var outerHeight = 350;
+        var margin = { left: 75, top: 20, right: 0, bottom: 60 };
+        var padding = 100;
+        var barPadding = 0.2;
+        var barPaddingOuter = 0.1;
+        var xColumn = "Volume";
+        var yColumn = "cliente";
+        var xAxisLabelOffset = 75;
+
+        var innerWidth  = outerWidth  - margin.left - margin.right;
+        var innerHeight = outerHeight - margin.top  - margin.bottom;
+
+        var svg = d3.select("#"+canvas_id).append("svg")
+            .attr("width",  outerWidth)
+            .attr("height", outerHeight)
+            .attr("viewBox", "-80 0 " + outerWidth + " " + outerHeight); 
+
+        svg.call(tip);
+
+        var g = svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var xAxisG = g.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + innerHeight + ")")
+
+        var yAxisG = g.append("g")
+            .attr("class", "y axis");
+
+        var xScale = d3.scale.linear()
+        .range([0, innerWidth - margin.right - margin.left], .1);
+
+        var yScale = d3.scale.ordinal()
+        .rangeRoundBands([innerHeight , 0], barPadding, barPaddingOuter);
+
+        var xAxis = d3.svg.axis().scale(xScale).orient("bottom")
+            .ticks(7)
+            .tickFormat(function(d) {return d /*+ "%"*/});
+
+        var yAxis = d3.svg.axis().scale(yScale).orient("left")
+            .outerTickSize(0);
+
+        xScale.domain([0, d3.max(chartData, function (d){ return d[xColumn]; })]);
+        yScale.domain(       chartData.map( function (d){ return d[yColumn]; }));
+        xAxisG.call(xAxis);
+        yAxisG.call(yAxis);
+
+        var bars = g.selectAll("rect").data(chartData);
+        bars.enter().append("rect")
+        .attr("height", yScale.rangeBand());
+        bars
+        .attr("x", 0)
+        .attr("class", "bar")
+        .attr("y",     function (d){ return yScale(d[yColumn]); })
+        .attr("width", function (d){ return xScale(d[xColumn]); })
+        .style("fill", function(d) {
+            return color(d.Volume); 
+        })
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide)
+
+    // horizontal bar labels
+    svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .selectAll(".textlabel")
+    .data(chartData)
+    .enter()
+    .append("text")
+    .attr("class", "textlabel")
+    .style("font-family", "Arial")
+    /*.attr("x", function(d){ return xScale(d["Percentage"]) + (xScale.range()/2);  })
+    .attr("y", function(d){ return yScale(d["LabelD3"]) - 3; })
+    .text(function(d){ return (d["Percentage"] + "%"); }); */
+
+
+    // X-axis labels
+   /* svg.append("text")
+    .attr("text-anchor", "middle")
+    .style("font-size", "13px")
+    .style("color", "#333333")
+    .attr("transform", "translate("+ (outerWidth/2) + "," +(outerHeight-(padding/4)) + ")")
+    .text("% of Households")
+    .style("font-family", "Arial"); */
+
+    //title for the chart 
+
+    /*svg.append("text")
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px")
+    .style("color", "#333333")
+    .attr("transform", "translate("+ (outerWidth/3.78) + "," +(outerHeight/30) + ")")
+    .text("Housing Tenure of DC Residents")
+    .style("font-family", "Arial");*/
+
+    //source
+
+    /*svg.append("text")
+    .attr("text-anchor", "middle")
+    .style("font-size", "13px")
+    .style("color", "#333333")
+    .attr("transform", "translate("+ (outerWidth/3.2) + "," +(outerHeight/1) + ")")
+    .text("Source: US Census ACS 5-year 2010-2014")
+    .style("font-family", "Arial")*/
+
+
+    svg.append("text")
+    .attr("text-anchor", "left")
+    .style("font-size", "10x")
+    .style("color", "#333333")
+    .attr("transform", "translate("+ (outerWidth/28) + "," +(outerHeight/12) + ")")
+    .text("Operario")
+    .style("font-family", "Arial")
+
+   // bars.exit().remove();
+
+
 }
 
 function BuildTableDia(chartData) {
