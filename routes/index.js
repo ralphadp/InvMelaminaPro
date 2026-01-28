ff212f65569577fb28974d43aa533a6d07163415
ralph.asendeteufrer <ralphadp@gmail.com>
2026-01-28 17:42:58 -0400

    set configuration data to presentation index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Melamina Pro' });
+    //Default data
+    let enterprise_data = {
+        title: 'Melacut G & C',
+        photo_enterprise:'images/IMG-20260114-WA0007.jpg',
+        celular1: '73758882',
+        celular2: ''
+    };
+
+    var DB = req.app.settings.DB;
+
+    let CollectionConfig = DB.collection("configuracion");
+    CollectionConfig.find().toArray().then((data) => {
+
+        data.forEach((configitem) => {
+              console.log(configitem);
+              enterprise_data = {
+                  title: configitem.nombre_app,
+                  photo_enterprise: configitem.foto,
+                  celular1: configitem.celular_1,
+                  celular2: configitem.celular_2
+              };
+              res.render('index', enterprise_data);
+        });
+    })
+    .catch(error => {
+          console.log(error.message);
+          res.render('index', enterprise_data);
+    })
 });
 
 module.exports = router;
